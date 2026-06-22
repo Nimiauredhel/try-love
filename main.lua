b7d9f286ecdc8dc81ea5eab5d6e4ba07d538bd2c
MickeyH@RPi5 <hozmickey@gmail.com>
2026-06-22 17:03:44 +0300

    partial raycasting implementation

--- main.lua
+++ main.lua
@@ -26,31 +26,19 @@ function update_scale()
 	tile_h = win_h / map_h
 end
 
-function on_edge(is_x)
-	hit_x = x
-	hit_y = y
-	temp_r = rectcol_r
-	temp_g = rectcol_g
-	temp_b = rectcol_b
-	rectcol_r = temp_b
-	rectcol_g = temp_r
-	rectcol_b = temp_g
-
-end
-
 function love.load()
+	pi = 3.14
+	tau = 6.28
+	love.keyboard.setKeyRepeat(true)
+	move_speed_min = 0.25
+	move_speed_max = 0.5
+	move_speed = move_speed_min
 	load_map()
 	update_scale()
 	view_mode = 0
 	player_x, player_y = map_w/2, map_h/2
-	w, h = 40, 40
-	x = love.math.random(win_w * 0.25, win_w * 0.75)
-	y = love.math.random(win_h * 0.25, win_h * 0.75)
-	dir_x, dir_y = 8, 8
-	if (love.math.random(0, 1) == 1) then dir_x = dir_x * -1 end
-	if (love.math.random(0, 1) == 1) then dir_y = dir_y * -1 end
-	rectcol_r, rectcol_g, rectcol_b = 0, 0.4, 0.4
-	hit_x, hit_y = 0, 0
+	player_dir = 0
+	player_angle = 0.0
 end
 
 function map_to_window(map_x, map_y)
@@ -60,22 +48,17 @@ function map_to_window(map_x, map_y)
 end
 
 function coord_to_cell(map_x, map_y)
-	cell = ((map_y-1)*map_w)+map_x
+	fmap_x, fmap_y = math.floor(map_x), math.floor(map_y)
+	cell = ((fmap_y-1)*map_w)+fmap_x
 	return cell
 end
 
 function love.update()
 	update_scale()
-	x = x + dir_x
-	y = y + dir_y
-	if (x > (win_w - w) or x < 0) then
-		on_edge()
-		dir_x = dir_x * -1
-	end
-	if (y > (win_h - h) or y < 0) then
-		on_edge()
-		dir_y = dir_y * -1
-	end
+	pdx = math.cos(player_angle)
+	pdy = math.sin(player_angle)
+	move_speed = move_speed - move_speed_min
+	if (move_speed < move_speed_min) then move_speed = move_speed_min end
 end
 
 function draw_map()
@@ -92,8 +75,12 @@ function draw_map()
 		end
 	end
 
-	love.graphics.setColor(0.8, 0.2, 0.2)
 	win_x, win_y = map_to_window(player_x, player_y)
+	love.graphics.setColor(0.2, 0.8, 0.2)
+	love.graphics.line(win_x, win_y, win_x+(pdx*tile_w), win_y+(pdy*tile_h))
+	win_x = win_x - tile_w/2
+	win_y = win_y - tile_h/2
+	love.graphics.setColor(0.8, 0.2, 0.2)
 	love.graphics.rectangle("fill", win_x, win_y, tile_w, tile_h)
 end
 
@@ -108,9 +95,9 @@ function draw_pov()
 	seg_start_x = { 0.0, 0.1, 0.35, 0.65, 0.9 }
 	seg_w = { 0.1, 0.25, 0.3, 0.25, 0.1 }
 	seg_corr = { 0.2, 0.2, 0.2, 0.2, 0.2 }
-	test_x_init = { -1, -1, 0, 1, 1 }
-	test_y_init = { 0, -1, -1, -1, 0 }
-	test_x_dir = { 0, 0, 0, 0, 0 }
+	test_x_init = { -1, 0, 0, 0, 1 }
+	test_y_init = { -1, -1, -1, -1, -1 }
+	test_x_dir = { -1, -1, 0, 1, 1 }
 	test_y_dir = { -1, -1, -1, -1, -1 }
 
 	mid = 3
@@ -119,8 +106,8 @@ function draw_pov()
 	for i = 1, 5 do
 		test_x, test_y = player_x + test_x_init[i], player_y + test_y_init[i]
 		repeat
-			dist[i] = dist[i] + 1
-			test_x, test_y = test_x + test_x_dir[i], test_y + test_y_dir[i]
+			dist[i] = dist[i] + move_speed_min 
+			test_x, test_y = test_x + (test_x_dir[i]*move_speed_min), test_y + (test_y_dir[i]*move_speed_min)
 		until (dist[i] > 200 or test_y < 1 or map[coord_to_cell(test_x, test_y)] == 1)
 		for s = 1, mult do
 			mod = s/mult
@@ -136,39 +123,145 @@ function draw_pov()
 	end
 end
 
+function draw_raycast(top_down)
+	hor_h = win_h * 0.5
+	pwx, pwy = map_to_window(player_x, player_y)
+	if (top_down) then
+	else
+		love.graphics.setColor(0.5, 0.5, 0.5)
+		love.graphics.rectangle("fill", 0, 0, win_w, hor_h)
+		love.graphics.setColor(0.25, 0.25, 0.25)
+		love.graphics.rectangle("fill", 0, hor_h, win_w, win_h)
+	end
+	dof_max = 32
+	ray_count = 60
+	ray_inc = tau/360
+	ray_angle = player_angle - (ray_inc * (ray_count/2))
+	for ray=1, ray_count do
+		if (ray_angle < 0.0) then ray_angle = ray_angle + tau
+		elseif (ray_angle > tau) then ray_angle = ray_angle - tau end
+
+		dof = 0
+		atan = -math.tan(ray_angle)
+		pxi, pxf = math.modf(player_x)
+		pyi, pyf = math.modf(player_y)
+		yoff = math.sin(ray_angle)
+		xoff = math.cos(ray_angle)
+		ry = player_y
+		rx = player_x
+		if (ray_angle > pi) then
+			--ry = math.floor(player_y) - 0.0001
+			--rx = (player_y - ry)*atan + player_x
+			--yoff = -1
+			--xoff = -yoff*atan
+		elseif (ray_angle < pi) then
+			--ry = math.floor(player_y) + 1
+			--rx = (player_y - ry)*atan + player_x
+			--yoff = 1
+			--xoff = -yoff*atan
+		elseif (ray_angle == 0 or ray_angle == pi) then
+			rx = player_x
+			ry = player_y
+			dof = dof_max
+		end
+
+		while (dof < dof_max) do
+			dof = dof + 1
+			if (rx < map_w+1
+			and ry < map_h+1
+			and rx > -1 and ry > -1) then
+				if (map[coord_to_cell(rx, ry)] == 1) then
+					if (player_x > rx) then
+						rx = math.ceil(rx)
+					else
+						rx = math.floor(rx)
+					end
+					if (player_y > ry) then
+						ry = math.ceil(ry)
+					else
+						ry = math.floor(ry)
+					end
+					if (top_down) then
+						wx, wy = map_to_window(rx, ry)
+						love.graphics.setColor(1.0, 0.0, 0.0)
+						love.graphics.line(pwx, pwy, wx, wy)
+					else
+						corr_angle = player_angle - ray_angle
+						if (corr_angle < 0) then
+							corr_angle = corr_angle + tau
+						elseif (corr_angle > tau) then
+							corr_angle = corr_angle - tau
+						end
+						dist_x = math.abs(player_x-rx)
+						dist_y = math.abs(player_y-ry)
+						mod = 1.0 - (dist_x/map_w)
+						if dist_y > dist_x then mod = 1.0 - (dist_y/map_h) end
+						mod = mod * math.cos(corr_angle)
+						love.graphics.setColor(0.5 * mod, mod, 0.2 * mod)
+						s_w = win_w/ray_count
+						s_h = win_h * mod
+						start_x = s_w * (ray-1)
+						love.graphics.rectangle("fill", start_x, hor_h-(s_h*0.5), s_w, s_h)
+					end
+				dof = dof_max
+				end
+			else dof = dof_max end
+			rx = rx + xoff
+			ry = ry + yoff
+		end
+	ray_angle = ray_angle + ray_inc
+	end
+end
+
 function love.draw()
 	if (view_mode == 0) then
 		draw_map()
-	else
-		draw_pov()
+		draw_raycast(true)
+	elseif (view_mode == 1) then
+		draw_raycast(false)
 	end
-	love.graphics.setColor(rectcol_r, rectcol_g, rectcol_b)
-	love.graphics.rectangle("fill", x, y, w, h)
-	love.graphics.line(x, y, hit_x, hit_y)
 end
 
-function love.keypressed(key, scancode, isrepeat)
-	move_x, move_y = 0, 0
-	if (key == "left") then
-		move_x, move_y = -1, 0
-	elseif (key == "up") then
-		move_x, move_y = 0, -1
-	elseif (key == "right") then
-		move_x, move_y = 1, 0
-	elseif (key == "down") then
-		move_x, move_y = 0, 1
+function move_player(dir)
+	move_x = pdx * move_speed * dir
+	move_y = pdy * move_speed * dir
+
+	if (dir == player_dir) then
+		move_speed = move_speed + move_speed_min * 10
 	else
-		if (key == "v") then
-			view_mode = view_mode + 1
-			if (view_mode > 1) then view_mode = 0 end
-		end
-		return
+		move_speed = move_speed_min
+	end
+
+	if (move_speed > move_speed_max) then
+		move_speed = move_speed_max
 	end
 	
 	player_x, player_y = player_x + move_x, player_y + move_y
 	if (player_x > map_w-1) or (player_x < 2) or (player_y > map_h-1) or (player_y < 2)
 		or map[coord_to_cell(player_x, player_y)] == 1 then
 		player_x, player_y = player_x - move_x, player_y - move_y
+		move_speed = move_speed_min
 	end
 
+end
+
+function love.keypressed(key, scancode, isrepeat)
+	if (key == "up") then
+		move_player(1)
+	elseif (key == "down") then
+		move_player(-1)
+	else
+		move_speed = move_speed_min
+	end
+
+	if (key == "left") then
+		player_angle = player_angle - (tau/180)
+		if (player_angle < 0.0) then player_angle = tau end
+	elseif (key == "right") then
+		player_angle = player_angle + (tau/180)
+		if (player_angle > tau) then player_angle = 0.0 end
+	elseif (key == "v") then
+		view_mode = view_mode + 1
+		if (view_mode > 1) then view_mode = 0 end
+	end
 end
